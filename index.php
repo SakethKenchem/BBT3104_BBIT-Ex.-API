9e66c062596af17efd8aa75963a10bedfde50fa2
SakethKenchem <saketh.kenchem@strathmore.edu>
2024-12-10 10:13:22 +0300

    Add PHPMailer library and fixed css for bootstrap jumbotron

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
 
 // Importing content from an external file
-require_once "classes/layout.php";
-require_once "contents/contents.php";
+require_once "autoload.php";
 
-// Creating an instance of a class
-$ObjLayout = new layout();
-$ObjContent = new contents();
 
 // Invoke a method
 $ObjLayout->heading();
